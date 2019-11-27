0c7b9c89e6efec490027bbe42fe1e58ef2c7e69b
kan_a_17 <kan.artem.web.dev@gmail.com>
2019-11-27 20:28:14 +0500

    init

---
+++ src/app/auth.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+  ) {
+    setInterval(() => {
+      this.isAdmin()
+        .subscribe((res) => {
+          if (!res['ok']) {
+            localStorage.clear();
+            router.navigate(['/login']);
+          }
+        });
+    }, 5000);
+  }
+
+  isAdmin() {
+    return this.http.post('/handle.php', {
+      action: 'checkauth',
+      key: this.getKey(),
+    });
+  }
+
+  logout() {
+    localStorage.removeItem('key');
+  }
+
+  checkAdmin(password: string) {
+    return this.http.post('/handle.php', { action: 'auth', password });
+  }
+
+  setKey(key: string) {
+    localStorage.setItem('key', key);
+  }
+
+  getKey() {
+    return localStorage.getItem('key');
+  }
+}
